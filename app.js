3651d7c1012b92e08908787bea199a921db11d4d
saad <saadtariq0812@gmail.com>
2022-06-06 22:17:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/*const httpee = require('http');
+const server = httpee.createServer((req, res) => {
+    console.log(req);
+    res.write("mir hamza is big gay unless he comes for dotu");
+    res.end("!");
+})
+
+server.listen(5000);
+*/
+
+const ding = require('lodash');
+const s = [1,[2,[3,[4]]]];
+const f = ding.flattenDeep(s);
+console.log(f);
